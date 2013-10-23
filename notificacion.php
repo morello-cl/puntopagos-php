0b5ab075b2383e67dd1f3dbcad983fea5f86c347
Marco Orellana Olivares <morello.cl@gmail.com>
2013-10-23 15:46:08 -0300

    Se agrega licencia MIT

--- notificacion.php
+++ notificacion.php
@@ -1,8 +1,25 @@
 <?php
         /*
-        @desc "Ejemplo procedural para ilustrar una correcta creaciÃ³e transaccion en ambiente sandbox de Punto Pagos"
-        @autor Marco Orellana Olivares <morello.cl@gmail.com>
-        @inputs $_POST['pp_trx_id'],$_REQUEST['pp_monto'], $_REQUEST['pp_medio_pago'], $_REQUEST['pp_detalle'] (opcional). Estos podrian estar tambien en sesion.
+        The MIT License (MIT)
+
+        Copyright (c) 2013 Marco Antonio Orellana Olivares <morello.cl@outlook.com>
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy of
+        this software and associated documentation files (the "Software"), to deal in
+        the Software without restriction, including without limitation the rights to
+        use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+        the Software, and to permit persons to whom the Software is furnished to do so,
+        subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+        FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+        COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+        IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+        CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
         */
         $config = parse_ini_file('puntopagos.ini', 1);
         $PUNTOPAGOS_URL = $config['puntopagos']['url'];
